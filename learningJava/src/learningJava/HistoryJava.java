dc79745532cd09bcbf16f4d7af9b847052a6708f
CookieSnack02 <pjoa580@gmail.com>
2026-02-13 23:45:18 -0300

    First exercise

---
+++ learningJava/src/learningJava/HistoryJava.java
@@ -0,0 +1,36 @@
+package learningJava;
+
+public class HistoryJava {
+
+	public static void main(String[] args) {
+
+		/*
+		 ** 
+		 * Java emerged** as an heir to the C and C++ languages.
+		 * 
+		 * The **C language was** created to be efficient and to replace *assembly*
+		 * language *in* systems development. The C language was a game changer in terms
+		 * of gaining ground between abandoning one language in favor of another. For
+		 * example, FORTRAN was used to write scientific programs efficiently, but it
+		 * was not good for coding systems. BASIC was easy to learn, but not as
+		 * powerful. Assembly was the high level of program production, but it was not
+		 * easy to learn and not as efficient.
+		 * 
+		 * The **C++ language** was created to meet the needs of C in **the** **1980s**.
+		 * The word that defines the creation of the C++ language is **complexity**.
+		 * Complexity was what led to the creation of the language, mainly due to the
+		 * modularity required as a result of large lines of code for the programmer to
+		 * work with. Thus, the language was created with **a focus on object-oriente**
+		 * .
+		 ** 
+		 * Java**, on the other hand, was created in **1991 by** **Sun Microsystems,
+		 * INC**. with the aim of meeting another apparent need: **platform
+		 * independence**.
+		 * 
+		 */
+
+		System.out.println();
+		
+	}
+
+}
