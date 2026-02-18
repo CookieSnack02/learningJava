b8fa58028c1fd1a4e5c747fe19525dcff18dee69
Jmanzioli <manzioli0808@gmail.com>
2026-02-18 00:37:52 -0300

    LeetCode - Palindrome Number (#09)

---
+++ learningJava/src/learningJava/Palindrome.java
@@ -0,0 +1,54 @@
+package learningJava;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+
+	    public boolean isPalindrome(int x) {
+	        
+	        if (x<0){
+	            return false; //constraint limit -> negative numbers
+	        }
+
+	            int numeroInvertido = 0;
+	            int copiaNumero = x;
+
+	        while (x>0){
+	            
+	            int verificador = x % 10; 
+
+	            numeroInvertido = numeroInvertido * 10 + verificador;
+
+	            x = x / 10;
+
+	        }
+
+
+	            return copiaNumero == numeroInvertido;
+	    }
+	
+	
+	
+	public static void main(String[] args) {
+		int numero;
+		Scanner scan = new Scanner(System.in);
+		
+	 
+		
+		System.out.println("Digite um número: ");
+		numero = scan.nextInt();
+		
+		Palindrome p = new Palindrome();
+		
+		if(p.isPalindrome(numero)) {
+			System.out.println("É palíndromo");
+		} else {
+			System.out.println("Não é palíndromo!");
+		}
+		
+		
+
+	}
+
+}
